ad7b3b6f562e1c1d030307120344dcf31beed140
lukaskwkw <lukaskwkw@gmail.com>
2016-01-05 11:13:52 +0100

    debuggng on heroku

--- app/routes/uploadFile.js
+++ app/routes/uploadFile.js
@@ -2,7 +2,7 @@ var multer = require("multer");
 var fs = require("fs");
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null,  process.cwd() + './public')
+		cb(null,  process.cwd() + '/public')
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname)
